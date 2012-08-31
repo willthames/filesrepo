c9e70dda7dc946bce1ea49d11d2bd64472c2a759
willthames <will.thames@suncorp.com.au>
2012-08-31 14:37:53 +1000

    Repository list now comes from config Repository pages list known files Unknown repositories are handled

--- filesrepo.py
+++ filesrepo.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
 import os
+import yaml
 
 app = Flask(__name__)
 
 app.config.from_object('config.Config')
 
+with file('repos.yaml') as f:
+    repos = yaml.load(f)
+
 @app.route('/')
 def index(): 
-    repos = os.listdir(app.config['REPO_DIR'])
     return render_template('index.html', repos=repos)
+
+@app.route('/<reponame>/')
+def repo(reponame):
+    repopath = os.path.join(app.config['REPO_DIR'], reponame)
+    if not os.path.exists(repopath):
+        return render_template('repo_not_found.html', reponame=reponame), 404
+    return render_template('repo.html', reponame=reponame,
+        files=os.listdir(repopath))
+
+@app.route('/<reponame>/<filename>')
+def file(reponame, filename):
+    return os.path.join(app.config['REPO_DIR'], reponame, filename)
 
 if __name__ == '__main__':
     app.run()
